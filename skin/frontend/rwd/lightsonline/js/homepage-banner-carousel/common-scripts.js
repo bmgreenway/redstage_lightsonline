0e60bd21316696741d5887630b0aa344727c9b52
Kailashlambe <kailashlambe7@gmail.com>
2015-09-09 18:25:03 +0530

    HomePage-dev: Dev issue fixed

--- skin/frontend/rwd/lightsonline/js/homepage-banner-carousel/common-scripts.js
+++ skin/frontend/rwd/lightsonline/js/homepage-banner-carousel/common-scripts.js
@@ -57,3 +57,44 @@ if(navigator.appName == 'Microsoft Internet Explorer' ){
 
 })(jQuery)
 
+
+//Mobile view toggle effect
+jQuery(document).ready(function() {
+	jQuery('#main-lightfinder').hide();
+	jQuery('#footer_shop_links').hide();
+	jQuery('#footer_account_links').hide();
+	jQuery('#footer_aboutus_links').hide();
+	jQuery('#footer_learn_links').hide();
+	jQuery('#home_page_heading_section').hide();
+	
+		jQuery( ".show_hide_toggle" ).click(function() { 
+			var toggleDiv = "";
+			var toggleClass = '';
+			toggleClassName = jQuery(this);
+			toggleDiv = toggleClassName.attr('dataparam');	
+			
+			jQuery('#'+toggleDiv).toggle("slow", function () {
+				//for only light finder dive to display show hide option
+				if(jQuery(this).is(':visible')){
+					//for light finder and home_page_heading_section text change only
+					if(toggleDiv == "main-lightfinder" || toggleDiv == "home_page_heading_section")
+					{
+						toggleClassName.html('Hide');
+					}
+					toggleClassName.removeClass('show_hide_toggle magic_arrow_down');	
+					toggleClassName.addClass('show_hide_toggle magic_arrow_up');
+				}else{	
+					//for light finder and home_page_heading_section text change only
+					if(toggleDiv == "main-lightfinder" || toggleDiv == "home_page_heading_section")
+					{
+						toggleClassName.html('Show');
+					}				
+					toggleClassName.removeClass('show_hide_toggle magic_arrow_up');					
+					toggleClassName.addClass('show_hide_toggle magic_arrow_down');
+				}			
+			}); 
+			return false;
+		});
+});
+
+
