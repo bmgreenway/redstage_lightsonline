9820b87d031d89007f0d50b3b4f169633ad385fc
Raman Jawade <rjawade@redstage.com>
2015-09-23 21:30:58 +0530

    changes for product page, homepage, category page

---
+++ app/code/local/Redstage/ProductAttributes/sql/redstage_productattributes_setup/upgrade-1.0.5-1.0.6.php
@@ -0,0 +1,37 @@
+<?php
+$installer = $this;
+
+$installer->startSetup();
+
+
+/* Add attribute group to attribute set */
+$productAttributesInfo =
+array(
+	'energy_guide_identifier' => array (
+	  'group'                      => 'Product Options',
+	  'type'                       => 'varchar',
+	  'label'                      => 'Energy Guide Identifier',
+	  'input'                      => 'text',
+	  'required'                   => '0',
+	  'user_defined'               => '1',
+	  'global'                     => '1',
+	  'visible'                    => '1',
+	  'searchable'                 => '0',
+	  'filterable'                 => '0',
+	  'comparable'                 => '0',
+	  'visible_on_front'           => '0',
+	  'is_html_allowed_on_front'   => '0',
+	  'filterable_in_search'       => '0',
+	  'used_in_product_listing'    => '0',
+	  'is_configurable'            => '0',
+	  'visible_in_advanced_search' => '0',
+	  'position'                   => '1',
+	  'used_for_promo_rules'       => '0' 
+    ),    
+);
+
+foreach ($productAttributesInfo as $attributeCode => $attributeParams) {
+    $installer->addAttribute(Mage_Catalog_Model_Product::ENTITY, $attributeCode, $attributeParams);
+}
+
+$installer->endSetup();
