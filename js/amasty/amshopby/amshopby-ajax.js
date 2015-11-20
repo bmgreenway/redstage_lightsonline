e13cf147bf3c82941ed6a681bd26c95f94b468e3
Raman Jawade <rjawade@redstage.com>
2015-11-20 15:43:47 +0530

    changes related to PROGLIGHTPROJ-124 and PROGLIGHTPROJ-118

--- js/amasty/amshopby/amshopby-ajax.js
+++ js/amasty/amshopby/amshopby-ajax.js
@@ -18,12 +18,6 @@ function amshopby_ajax_init(){
             if ((p.hasClassName('amshopby-cat') && !p.hasClassName('amshopby-cat-multi')) || p.hasClassName('amshopby-clearer')){
                 return;
             }
-			var checkProductGridimg = e.up().up().up();
-			var checkProductGridname = e.up().up().up().up();
-			
-			if (checkProductGridimg.hasClassName('products-grid') || checkProductGridname.hasClassName('products-grid')) {
-				return false;
-			}
 			
             e.onclick = function(){
                 if (this.hasClassName('checked')) {
@@ -290,7 +284,7 @@ document.observe("dom:loaded", function(event) {
 
 });
 
-var amshopby_toolbar_selector = '.amshopby-page-container';
+var amshopby_toolbar_selector = 'div.toolbar';
 var amshopby_scroll_to_products = false;
 
 function amshopby_external(){
