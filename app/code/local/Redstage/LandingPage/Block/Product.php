b72045c448e213b831f10fc4cb10e4b9ebe9724e
Raman Jawade <rjawade@redstage.com>
2015-11-27 16:26:43 +0530

    changes related to PROGLIGHTRET-16

---
+++ app/code/local/Redstage/LandingPage/Block/Product.php
@@ -0,0 +1,8 @@
+<?php
+class Redstage_LandingPage_Block_Product extends Mage_Core_Block_Template
+{
+	public function _construct()
+	{
+		$this->setTemplate('redstage_landingpage/product.phtml');
+	}
+}
